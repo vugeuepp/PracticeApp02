7b72681832f975b418424674e33242a034838aff
Yamada Naoki <vugeuepp@gmail.com>
2021-07-27 17:29:38 +0900

    firstcommit

---
+++ web/result.php
@@ -0,0 +1,17 @@
+<?php 
+
+require('../app/functions.php');
+
+include('../app/_parts/_header.php');
+
+
+?>
+
+<p>Message added!</p>
+<p><a href="index.php">Go back</a></p>
+
+<?php
+
+include('../app/_parts/_footer.php');
+
+?>
